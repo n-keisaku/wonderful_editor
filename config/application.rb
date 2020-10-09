5addea26e13471440f8887ca0047af0e2f8119a6
n-keisaku <0nakamura0k@gmail.com>
2020-10-10 05:47:08 +0900

    余計なファイルを作らないよう修正

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,18 @@ module WonderfulEditor
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec、
+                        viwe_specs: false,
+                        routing_specs: false,
+                        helper_specs: false,
+                        controller_specs: false,
+                        request_specs: true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +43,6 @@ module WonderfulEditor
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.api_only = true
   end
 end
