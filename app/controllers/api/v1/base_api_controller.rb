0bde136af3668eef0185a73fe21adc553475db73
n-keisaku <0nakamura0k@gmail.com>
2020-12-12 08:46:00 +0900

    既存APIの修正

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::BaseApiController < ApplicationController
   # current_user のダミーコード
-  def current_user
-    # ||は@current_user が存在しないとき右辺を代入する
-    @current_user ||= User.first
-  end
+  # def current_user
+  #   # ||は@current_user が存在しないとき右辺を代入する
+  #   @current_user ||= User.first
+  # end
+  # メソッドのエイリアスで別名を定義する alias_method 新メソッド名, 旧メソッド名
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
 end
